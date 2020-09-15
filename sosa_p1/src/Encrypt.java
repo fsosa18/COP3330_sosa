aa8f0d9593fb605d94f3ce18ade5c835020bfcb4
fsosa18 <fernansosa18@gmail.com>
2020-09-14 21:01:35 -0400

    submit first program

---
+++ sosa_p1/src/Encrypt.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Encrypt
+{	
+	//declaring constant size of input & output
+	public static final int SIZE = 4;
+
+	//Overall function encrypting the number
+	public static String encrypt(String input)
+	{
+		//puts int into an array from the string
+		int tempNum = Integer.parseInt(input);
+		int num[] = new int[SIZE];
+		for(int i = SIZE - 1; i >= 0; i--)
+		{
+			int tempDig = tempNum % 10;
+			tempNum /= 10;
+			num[i] = changeDig(tempDig);
+		}
+
+		//swaps digits
+		for(int i = 0; i < SIZE/2; i++)
+		{
+			num = swapDig(num, i, i+2);
+		}
+
+		//returns contents of array as a string
+		return arrToString(num);
+	}
+
+	//Encrypts each digit, adding 7, getting raminder after dividing by 10
+	public static int changeDig(int input)
+	{
+		int num = input + 7;
+		num %= 10;
+		return num;
+	}
+
+	//Swaps required digits(1 & 3, 2 & 4) to complete encryption
+	public static int[] swapDig(int[] input, int ind1, int ind2)
+	{
+		int [] arr = input;
+		int temp = arr[ind1];
+		arr[ind1] = arr[ind2];
+		arr[ind2] = temp;
+		return arr;
+	}
+
+	//Changes the array to a string, in order to return the required type
+	public static String arrToString(int[] arr)
+	{
+		String num = "";
+		for(int i = 0; i < SIZE; i++)
+		{
+			String dig = Integer.toString(arr[i]);
+			num += dig;
+
+		}
+		return num;
+	}
+
+	public static void main(String [] args)
+	{
+		System.out.println(encrypt("1234")); //0189
+		System.out.println(encrypt("0000")); //7777
+		System.out.println(encrypt("4321")); //9810
+	}
+}
